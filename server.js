1edc6913e3fc5584a557eb8f43ef3402d5bf9182
vedant-11 <v.vedantsingh.11@gmail.com>
2022-11-01 18:57:18 +0530

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const tokenRoutes = require("./routes/tokenRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
 const balanceRoutes = require("./routes/balanceRoutes");
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use((req, res, next) => {
   next();
 });
+//
+app.use(cors());
 
 // routes
 app.use("/api/token", tokenRoutes);
