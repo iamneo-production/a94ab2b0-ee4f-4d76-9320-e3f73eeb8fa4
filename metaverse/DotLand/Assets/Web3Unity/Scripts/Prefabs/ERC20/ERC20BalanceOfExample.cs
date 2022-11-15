f06ee0083d6107a778bf7304cd5445f0036ef0ae
vedant-11 <v.vedantsingh.11@gmail.com>
2022-11-15 21:16:54 +0530

    added scenes

--- metaverse/DotLand/Assets/Web3Unity/Scripts/Prefabs/ERC20/ERC20BalanceOfExample.cs
+++ metaverse/DotLand/Assets/Web3Unity/Scripts/Prefabs/ERC20/ERC20BalanceOfExample.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Numerics;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ERC20BalanceOfExample : MonoBehaviour
 {
+    [SerializeField]
+    private TMP_Text _title;
+    private BigInteger balanceOf;
     async void Start()
     {
-        string chain = "ethereum";
-        string network = "mainnet";
-        string contract = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-        string account = "0x000000ea89990a17Ec07a35Ac2BBb02214C50152";
+        string chain = "polygon";
+        string network = "testnet";
+        string contract = "0x461089f45eDD7dAB76614651711b021439ca3835";
+        string account = "0x8E7A5aF22aB3B208e2E2DD9cc2E0a26869c1dd17";
+        
 
-        BigInteger balanceOf = await ERC20.BalanceOf(chain, network, contract, account);
+        balanceOf = await ERC20.BalanceOf(chain, network, contract, account);
         print(balanceOf); 
+    }
+
+    public void SetTitle()
+    {
+        _title.text = balanceOf.ToString();
     }
 }
